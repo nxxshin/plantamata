9f47cedc9a7d00aaf0addc0d67197057a6286771
nxxshin <noxshin@proton.me>
2024-08-24 22:42:09 +0200

    worldgen now works, press F to reload + camera zoom with Z and X

--- plantamata.cpp
+++ plantamata.cpp
@@ -7,9 +7,8 @@
 int main(void)
 {
     // Initialization
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-
+    const int screenWidth = 1200;
+    const int screenHeight = 720;
     
 
     InitWindow(screenWidth, screenHeight, "Plantamata");
@@ -29,23 +28,31 @@ int main(void)
     camera.rotation = 0;
     camera.zoom = 1;
 
+    
+    
+  
+
     // Main game loop
     while (!WindowShouldClose())
     {
         // Update
         float deltaTime = GetFrameTime();
         player.Update(deltaTime);
+       
 
         // Draw
         BeginDrawing();
         BeginMode2D(camera); 
-
-
         
-        ClearBackground(Color {240, 240, 240, 226});
+        ClearBackground(WHITE);
         drawWorld();
         player.Draw();
         camera.target = player.position;
+
+        if (IsKeyPressed(KEY_F)) {worldGen();}
+        if (IsKeyDown(KEY_Z)) {camera.zoom += 0.05;}
+        if (IsKeyDown(KEY_X) && camera.zoom > 0.25) {camera.zoom -= 0.05;}
+
 
         DrawFPS(10, 10);
         EndMode2D();
--- src/worldgen/worldgen.cpp
+++ src/worldgen/worldgen.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include "raylib.h"
 
-const int GRID_SIZE = 32;
+const int GRID_SIZE = 64;
 const int SIZE = 32;
 Texture2D tileset;
 
 struct Cell {
     float x;
     float y;
-    int tileNumber;
+    float sourceX;
+    float sourceY;
 };
 
-float sourceX;
-float sourceY;
+float sourceX = 16*11;
+float sourceY = 16*3;
 
 
 Cell grid[GRID_SIZE][GRID_SIZE];
@@ -21,6 +22,33 @@ void loadTileset() {
 tileset = LoadTexture("resources/world/tilesheet.png");
 };
 
+void assignTile(float &sourceX, float &sourceY) {
+
+    float randNumber = rand() % 25;
+    struct Grass {
+        int x = 16*11;
+        int y = 3*16;
+    };
+
+    Grass grass;
+
+    int flower1 = 16*10;
+    int flower2 = 16*12;
+
+
+    sourceX = (randNumber == 1) ? grass.x : grass.x;
+    sourceY = (randNumber == 1) ? grass.y : grass.y;
+    sourceX = (randNumber == 2) ? flower1 : sourceX;
+    sourceY = (randNumber == 2) ? 0 : sourceY;
+    sourceX = (randNumber == 0) ? flower2 : sourceX;
+    sourceY = (randNumber == 0) ? 0 : sourceY;
+    sourceX = (randNumber == 3) ? grass.x + 32 : sourceX;
+    sourceY = (randNumber == 3) ? grass.y: sourceY;
+    sourceX = (randNumber == 4) ? grass.x + 48 : sourceX;
+    sourceY = (randNumber == 4) ? grass.y: sourceY;
+
+}
+
 void createGrid(int GRID_SIZE) {
     for (int i = 0; i < GRID_SIZE; i++) {
         for (int j = 0; j < GRID_SIZE; j++) {
@@ -28,43 +56,40 @@ void createGrid(int GRID_SIZE) {
         float y = j * 32;
         grid[i][j].x = x;
         grid[i][j].y = y;
+        assignTile(sourceX, sourceY);
+        grid[i][j].sourceX = sourceX;
+        grid[i][j].sourceY = sourceY;
+
+       
         
     }
 }
 }
 
-void assignTile(float &sourceX, float &sourceY) {
-    float randNumber = rand() % 25;
-    sourceY = 0;
-    sourceX = (randNumber == 1) ? 0 : 0;
-    sourceX = (randNumber == 2) ? 48 : sourceX;
-    sourceX = (randNumber == 0) ? 16*14 : sourceX;
-    sourceY = (randNumber == 0) ? 16: sourceY;
 
-}
 
 void drawWorld() {
 
 
     float size = 32;
-    Color color = LIGHTGRAY;
+    Color color = {160, 205, 62, 126};
 
     for (int i = 0; i < GRID_SIZE; i++) {
         for (int j = 0; j < GRID_SIZE; j++) {
            float x =  grid[i][j].x;
            float y = grid[i][j].y;
     Vector2 position = {x,y};
-    // DrawRectangle(x,y, size, size, color); 
+    DrawRectangle(x,y, size, size, color); 
  
 
-    Rectangle drawSource = {sourceX, sourceY, 16, 16};
+    Rectangle drawSource = {grid[i][j].sourceX, grid[i][j].sourceY, 16, 16};
     Rectangle drawDest = {x, y, 32, 32};
 
-    assignTile(sourceX, sourceY);
+    // assignTile(sourceX, sourceY);
 
     DrawTexturePro(tileset, drawSource, drawDest, {0, 0}, 0, WHITE);
     
-    DrawRectangleLines(x, y, size, size, BLUE);
+    // DrawRectangleLines(x, y, size, size, BLUE);
 
     }}
 }
